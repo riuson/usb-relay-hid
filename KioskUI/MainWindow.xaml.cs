58f2cf38f5d83ae5141681ecc7ee31201f8bf234
Vladimir <riuson@gmail.com>
2023-09-08 22:04:29 +0500

    Add visual controls

--- KioskUI/MainWindow.xaml.cs
+++ KioskUI/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using ReactiveUI;
+﻿using System.Windows;
+using System.Windows.Controls;
+using ReactiveUI;
 
 namespace KioskUI {
     /// <summary>
@@ -9,6 +11,71 @@ namespace KioskUI {
             this.InitializeComponent();
 
             this.ViewModel = new AppViewModel();
+
+            for (var i = 0; i < 8; i++) {
+                // Channel labels.
+                var channelLabelBorder = new Border {
+                    Child = new Label {
+                        Content = new TextBlock {
+                            Text = $"Channel {i}",
+                            HorizontalAlignment = HorizontalAlignment.Center,
+                            VerticalAlignment = VerticalAlignment.Center
+                        }
+                    },
+                    HorizontalAlignment = HorizontalAlignment.Center,
+                    VerticalAlignment = VerticalAlignment.Center
+                };
+                this.GridMain.Children.Add(channelLabelBorder);
+                Grid.SetRow(channelLabelBorder, i + 2);
+                Grid.SetColumn(channelLabelBorder, 0);
+
+                // Open buttons.
+                var buttonOpenBorder = new Border {
+                    Child = new Button {
+                        Content = new TextBlock {
+                            Text = "Open",
+                            HorizontalAlignment = HorizontalAlignment.Center,
+                            VerticalAlignment = VerticalAlignment.Center
+                        }
+                    },
+                    HorizontalAlignment = HorizontalAlignment.Stretch,
+                    VerticalAlignment = VerticalAlignment.Stretch
+                };
+                this.GridMain.Children.Add(buttonOpenBorder);
+                Grid.SetRow(buttonOpenBorder, i + 2);
+                Grid.SetColumn(buttonOpenBorder, 1);
+
+                // State labels.
+                var stateLabelBorder = new Border {
+                    Child = new Label {
+                        Content = new TextBlock {
+                            Text = $"Channel {i}",
+                            HorizontalAlignment = HorizontalAlignment.Center,
+                            VerticalAlignment = VerticalAlignment.Center
+                        }
+                    },
+                    HorizontalAlignment = HorizontalAlignment.Center,
+                    VerticalAlignment = VerticalAlignment.Center
+                };
+                this.GridMain.Children.Add(stateLabelBorder);
+                Grid.SetRow(stateLabelBorder, i + 2);
+                Grid.SetColumn(stateLabelBorder, 2);
+
+                var buttonCloseBorder = new Border {
+                    Child = new Button {
+                        Content = new TextBlock {
+                            Text = "Close",
+                            HorizontalAlignment = HorizontalAlignment.Center,
+                            VerticalAlignment = VerticalAlignment.Center
+                        }
+                    },
+                    HorizontalAlignment = HorizontalAlignment.Stretch,
+                    VerticalAlignment = VerticalAlignment.Stretch
+                };
+                this.GridMain.Children.Add(buttonCloseBorder);
+                Grid.SetRow(buttonCloseBorder, i + 2);
+                Grid.SetColumn(buttonCloseBorder, 3);
+            }
         }
     }
 }
